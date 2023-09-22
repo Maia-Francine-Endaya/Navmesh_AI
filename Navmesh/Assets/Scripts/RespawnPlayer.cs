0a460723c7f615415543a32cbaf48cee038df247
Maia-Francine-Endaya <88295922+Maia-Francine-Endaya@users.noreply.github.com>
2023-09-22 10:45:26 +0800

    Added respawning function

---
+++ Navmesh/Assets/Scripts/RespawnPlayer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnPlayer : MonoBehaviour
+{
+  [SerializeField] private Transform player;
+  [SerializeField] private Transform respawnPoint;
+
+  void OnTriggerEnter(Collider other)
+  {
+    player.transform.position = respawnPoint.transform.position;
+  }
+}
